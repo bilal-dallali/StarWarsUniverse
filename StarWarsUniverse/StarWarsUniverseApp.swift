2f84e81636d2df3be2344d74dc4e633e40def2a1
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2022-12-05 17:45:39 +0100

    Initial Commit

---
+++ StarWarsUniverse/StarWarsUniverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsUniverseApp.swift
+//  StarWarsUniverse
+//
+//  Created by Bilal Dallali on 05/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsUniverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
